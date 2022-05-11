01556a30819fbc49da3139ca3baaa4dc11bdc2ab
nihongo-deb <dal34@tpu.ru>
2022-05-12 02:23:29 +0700

    some additional in main

--- src/Main.kt
+++ src/Main.kt
@@ -3,12 +3,20 @@ fun main() {
     vector.randomFillMatrixInt(0, 10)
     println(vector)
 
-    var squareMatrix : Matrix = SquareMatrix(6)
+    var squareMatrix : Matrix = SquareMatrix(5)
     squareMatrix.randomFillMatrixDouble(0.0, 7.5)
     println(squareMatrix)
 
     var nullMatrix : Matrix = NullMatrix(5,5)
     println(nullMatrix)
 
-    
+    var diagonalMatrix : Matrix = DiagonalMatrix(5)
+    diagonalMatrix.randomFillMatrixInt(0, 15)
+    println(diagonalMatrix)
+
+    var unitMatrix : Matrix = UnitMatrix(5)
+    println(unitMatrix)
+
+    println("squareMatrix + unitMatrix:\n" + MatrixMath.sumMatrix(squareMatrix, unitMatrix))
+    println("squareMatrix * unitMatrix:\n" + MatrixMath.multiplyMatrix(squareMatrix, unitMatrix))
 }
